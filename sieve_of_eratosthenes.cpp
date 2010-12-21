628abdd2a8a42a219c4e4396f4af76144ca3cf9e
chaitanyav <me@chaitanyavellanki.com>
2010-12-21 00:17:58 -0800

    Clean up

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -15,23 +15,23 @@ using namespace std;
 
 
 int main() {
-  const int64_t n = 100000000;
-  bitset<n> sieve;
+  const int64_t N = 1000000000;
+  bitset<N> sieve;
 
   sieve.set(); // set all bits
   sieve.reset(0); // clearing the first bit
 
-  int64_t sqrt_n = (int64_t)sqrt(n) + 1;
+  int64_t sqrt_n = (int64_t)sqrt(N) + 1;
   for(int64_t num = 2; num < sqrt_n; num++) {
     if(sieve[num - 1] == 1) {
-      for(int64_t mnum = num * num; mnum <= n; mnum += num) {
+      for(int64_t mnum = num * num; mnum <= N; mnum += num) {
         sieve.reset(mnum - 1);
       }
     }
   }
 
   // using the sieve to print primes
-  //for(int64_t i = 2; i <= n; i++) {
+  //for(int64_t i = 2; i <= N; i++) {
     //if(sieve[i - 1] == 1) {
       //cout << i << endl;
     //}
